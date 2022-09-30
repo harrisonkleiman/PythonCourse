b1b67b2161b6d3b222d0e882bc1ab34ff4499597
harrisonkleiman <harrikleiman@gmail.com>
2022-09-29 22:43:00 -0400

    section 6 assignment complete

--- Section_06/assignment_01.py
+++ Section_06/assignment_01.py
@@ -14,6 +14,50 @@ words_to_aggregate = sys.argv[2:]
 
 # Your Code Below:
 
+# 1. Create a dictionary to store the words and their counts
+
+word_count = {}
+
+# 2. Loop through the files in the directory
+
+for file in os.listdir(directory_containing_files):
+
+# 3. Open each file
+
+    with open(os.path.join(directory_containing_files, file), "r") as f:
+# 4. Read the contents of the file
+
+        contents = f.read()
+# 5. Split the contents of the file into a list of words
+
+        words = contents.split()
+#6. Lopp through the words in the list
+
+        for word in words:
+            
+# 7. Check if the word is in the dictionary
+
+            if word in word_count:
+# 8. If the word is in the dictionary, increment the count
+
+                word_count[word] += 1
+# 9. If the word is not in the dictionary, add it to the dictionary with a count of 1
+    
+            else:
+                word_count[word] = 1
+# 10. Print the dictionary
+
+print(word_count)
+
+
+# 11. Run the program from the command line
+
+# 12. Test the program with the following command:
+
+# python3 assignment_01.py /Users/imtiazahmad/PycharmProjects/Assignments/Section_06/project_files hello Peter computer
+
+
+
 
 
 
